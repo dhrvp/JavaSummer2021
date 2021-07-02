590aa8fa7821aac1ac1b4a242611b4d67f9fb0f8
Dhruv <dpisal24@gmail.com>
2021-07-02 13:24:58 -0400

    Example

---
+++ 5Array/src/PizzaArrayTest.java
@@ -0,0 +1,17 @@
+
+public class PizzaArrayTest {
+	public static void main(String[] args) {
+		String[] DDhhruv;
+		DDhhruv = new String[4];
+		DDhhruv[0] = "Great";
+		DDhhruv[1] = "Courageous";
+		DDhhruv[2] = "Awesome";
+		DDhhruv[3] = "Marvelous";
+
+		System.out.println("Words to describe the Dhruv: ");
+		System.out.println(DDhhruv[0]);
+		System.out.println(DDhhruv[1]);
+		System.out.println(DDhhruv[2]);
+		System.out.println(DDhhruv[3]);
+		}
+		}
