7bceb9ff913be5b49cd470780015504303090bdf
Dhruv <dpisal24@gmail.com>
2021-07-02 19:11:24 -0400

    Homework Complete

---
+++ 6Loop/src/Homework6_2.java
@@ -0,0 +1,18 @@
+/**
+ * Print number 1 to 100 using while loop
+ * @author Dhruv
+ *
+ */
+public class Homework6_2 {
+public static void main(String[] args) {
+	int maxnum = 100;
+	int main = 1;
+	
+	while (main <= maxnum) {
+		System.out.println(main);
+		main++;
+	
+}
+}
+}
+
