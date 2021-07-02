e51ab39fe88f6c78f29cb0ed6d3d07c43698445b
Dhruv <dpisal24@gmail.com>
2021-07-02 13:26:18 -0400

    While Loop Example

---
+++ 6Loop/src/LoopEx.java
@@ -0,0 +1,13 @@
+
+public class LoopEx {
+	public static void main(String[] args) {
+		int max = 10;
+		int counter =1;
+		
+		while (counter <= max) {
+			System.out.println(counter);
+		counter++;
+	}
+	}
+	}
+
