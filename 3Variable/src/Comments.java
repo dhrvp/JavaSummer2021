f0b292531e995fb034d9787ba5312749470b5b5d
Dhruv <dpisal24@gmail.com>
2021-07-02 13:23:27 -0400

    Examples

---
+++ 3Variable/src/Comments.java
@@ -0,0 +1,13 @@
+
+/**
+ * This is class java doc.
+ * @author dhruvpisal
+ *
+ */
+public class Comments {
+ public static void main(String[] args) {
+	//This is a sample 
+	 	System.out.println("This is a sample program");
+ }
+ 
+}
---
+++ 3Variable/src/DataTypes.java
@@ -0,0 +1,30 @@
+
+public class DataTypes {
+ public static void main(String[] args) {
+	// int number variable with value
+	int speed = 100;
+	long bigNumber;
+	
+	//Assign value to var
+	bigNumber = 22000l;
+	
+	int someIntNumber;
+	
+	//Assigning value from other variable
+	someIntNumber = speed;
+	
+	float PI = 3.14f;
+	
+	String name = "Dhruv";
+	
+	char firstChar = 'a';
+	
+	boolean greaterNumber = true;
+	System.out.println("Car is running with speed " + speed);
+	
+	System.out.println("My name is " + name.toUpperCase());
+	
+	System.out.println(greaterNumber);
+
+}
+}
